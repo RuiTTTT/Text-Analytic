f7e6cbb555bf16ca0d431777e480d48da2899e2e
seraphimTT <767731843@qq.com>
2017-10-18 11:39:19 +0100

    fixed

--- P3_1.py
+++ P3_1.py
@@ -22,7 +22,7 @@ def n_occurance(word, bloblist):
 
 
 def idf(word, bloblist):
-    return math.log2(len(bloblist) / (1 + n_occurance(word, bloblist)))
+    return math.log10(len(bloblist) / (1 + n_occurance(word, bloblist)))
 
 
 def tfidf(word, blob, bloblist):
@@ -38,19 +38,19 @@ d2 = TextBlob(remove_punc(
 d3 = TextBlob(remove_punc(
     "Dry in many areas this afternoon & evening, but mostly cloudy, outbreaks of rain & drizzle. A few bright spells in places also. Highs 13/16 C"))
 d4 = TextBlob(remove_punc(
-    "Rather cloudy this aft. Dry in many places but there will be some sctd outbreaks of rain, & possibly  more persistent rain in parts of the North"))
+    "Rather cloudy this afternoon. Dry in many places but there will be some sctd outbreaks of rain, & possibly  more persistent rain in parts of the North"))
 d5 = TextBlob(remove_punc(
     "Rather cloudy, some sunny breaks developing in places. Dry in many areas, but there will be some shwry outbreaks of rain, mainly in North & NorthWest"))
 d6 = TextBlob(remove_punc(
-    "Mostly cloudy today. Some hazy sunnyspells. Mainly dry. Patchy light rain in far SouthWest this morn & isol patches of drzl near NorthWest coasts.High 13-16 C"))
+    "Mostly cloudy today. Some hazy sunnyspells. Mainly dry. Patchy light rain in far SouthWest this morning & isol patches of drzl near NorthWest coasts.High 13-16 C"))
 d7 = TextBlob(remove_punc(
     "Mild & breezy overnight. Scattered patches of rain & drzl at times - mainly over Muns, but many areas will stay dry. Lows 9 to 12 degrees."))
 d8 = TextBlob(remove_punc(
     "Breezy, misty & mostly cloudy this evening & tonight, with patches of rain & drizzle at times. Mild, lowest temperatures 9 to 12 C."))
 d9 = TextBlob(remove_punc(
-    "Dry in many areas this aft. but mostly cloudy. However patchy rain, drizzle and mist along Atlantic coastal counties will slowly extend East."))
+    "Dry in many areas this afternoon. but mostly cloudy. However patchy rain, drizzle and mist along Atlantic coastal counties will slowly extend East."))
 d10 = TextBlob(remove_punc(
-    "Dry in many areas this aft, with a few bright spells, but predominantly cloudy, with a little patchy rain or drizzle in places. 12/15 C."))
+    "Dry in many areas this afternoon, with a few bright spells, but predominantly cloudy, with a little patchy rain or drizzle in places. 12/15 C."))
 
 bloblist = [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10]
 
@@ -58,12 +58,16 @@ word_set_duplicate = []
 co = {}
 pmi = {}
 for blob in bloblist:
-    print(remove_stopwords(blob))
+    # print(remove_stopwords(blob))
     word_set_duplicate = word_set_duplicate + list(remove_stopwords(blob))
 
 word_set = set(word_set_duplicate)
 for worda in word_set:
+    if word_set_duplicate.count(worda) <= 1:
+        continue
     for wordb in word_set:
+        if word_set_duplicate.count(worda) <= 1:
+            continue
         count = 0
         for blob in bloblist:
             if worda != wordb:
@@ -76,6 +80,7 @@ for worda in word_set:
             word_set_duplicate.count(worda)) - math.log2(word_set_duplicate.count(wordb))
 print(sorted(pmi.items(), key=lambda x: x[1], reverse=True))
 
+total_tfidf = {}
 for i, blob in enumerate(bloblist):
     print("Top words in document {}".format(i + 1))
     scores_tf = {word: tf(word, blob) for word in remove_stopwords(blob)}
@@ -90,6 +95,18 @@ for i, blob in enumerate(bloblist):
     print(sorted_idf)
 
 total_tf = {}
+total_idf = {}
 for word in word_set:
     total_tf[word] = word_set_duplicate.count(word)
+    total_idf[word] = idf(word, bloblist)
+    total_tfidf[word] = total_tf[word] * total_idf[word]
 print(sorted(total_tf.items(), key=lambda x: x[1], reverse=True))
+sort_tfidf = sorted(total_tfidf.items(), key=lambda x: x[1], reverse=True)
+print(sort_tfidf)
+aaa = ''
+for item in word_set_duplicate:
+    aaa = aaa + ' ' + item
+print(total_tfidf.keys())
+for i in total_tfidf.values():
+    print(i * 10, end=',')
+print(total_tfidf.items())
